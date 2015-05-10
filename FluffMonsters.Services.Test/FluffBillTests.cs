6452d9fa537a13eba6840d05338a8e2ee629e849
Cade Kachelmeier <cade.kachelmeier@gmail.com>
2015-05-09 21:07:41 -0500

    Added discounts and coupons

---
+++ FluffMonsters.Services.Test/FluffBillTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+
+namespace FluffMonsters.Services.Test
+{
+    [TestFixture]
+    public class FluffBillTests
+    {
+        #region jsonStrings
+        string serviceMenu1 = @"
+[
+    {
+        'name': 'Day of Food',
+        'price': '$10.00'
+    },
+    {
+        'name': 'Day of Boarding',
+        'price': '$49.00'
+    },
+    {
+        'name': 'Chew Toy',
+        'price': '$5.00'
+    }
+]";
+
+        string customerItemsMultipleItems = @"
+[
+    'Day of Food',
+    'Day of Boarding',
+    'Chew Toy',
+    'Day of Food',
+    'Chew Toy',
+    'Day of Boarding',
+    'Day of Boarding',
+    'Day of Boarding',
+    'Day of Food',
+    'Day of Food',
+    'Day of Food'
+]";
+
+        private string customerItemsSingleDayOfFood = @"
+[
+            'Day of Food'
+]";
+        private string customerItemsSingleDayOfBoarding = @"
+[
+            'Day of Boarding'
+]";
+        private string customerItemsSingleChewToy = @"
+[
+            'Chew Toy'
+]";
+        private string customerItemsThreeDaysOfBoarding = @"
+[
+            'Day of Boarding',
+            'Day of Boarding',
+            'Day of Boarding'
+]";
+        private string customerItemsUnknownItem = @"
+[
+            'Unknown'
+]";
+
+        private string customerItemsSingleDayWithCoupon = @"
+[
+            'Day of Boarding',
+            '$5.00 Coupon'
+]";
+        private string customerItemsTwoDaysWithCoupon = @"
+[
+            'Day of Boarding',
+            'Day of Boarding',
+            'Day of Boarding',
+            '$5.00 Coupon'
+]";
+        #endregion
+
+        #region test variables
+
+        //private JArray serviceMenu1;
+        //private JArray customerItemsMultipleItems;
+        //private JArray customerItemsSingleDayOfFood;
+        //private JArray customerItemsSingleDayOfBoarding;
+        //private JArray customerItemsSingleChewToy;
+        //private JArray customerItemsTwoDaysOfBoarding;
+        //private JArray customerItemsUnknownItem;
+        #endregion
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            //serviceMenu1 = JsonConvert.DeserializeObject<JArray>(serviceMenuJson1);
+            //customerItemsMultipleItems = JsonConvert.DeserializeObject<JArray>(customerItemsJson1);
+            //customerItemsSingleDayOfFood = JsonConvert.DeserializeObject<JArray>(customerItemsJson2);
+            //customerItemsSingleDayOfBoarding = JsonConvert.DeserializeObject<JArray>(customerItemsJson3);
+            //customerItemsSingleChewToy = JsonConvert.DeserializeObject<JArray>(customerItemsJson4);
+            //customerItemsTwoDaysOfBoarding = JsonConvert.DeserializeObject<JArray>(customerItemsJson5);
+            //customerItemsUnknownItem = JsonConvert.DeserializeObject<JArray>(customerItemsJson6);
+        }
+
+        [Test]
+        public void GetBillTotal_Single_Day_Of_Food_Returns_Price()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsSingleDayOfFood);
+            bill.getBillTotal().Should().Be(10);
+        }
+
+        [Test]
+        public void GetBillTotal_Single_Day_Of_Boarding_Returns_Price()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsSingleDayOfBoarding);
+            bill.getBillTotal().Should().Be(49);
+        }
+
+        [Test]
+        public void GetBillTotal_Single_Chew_Toy_Returns_Price()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsSingleChewToy);
+            bill.getBillTotal().Should().Be(5);
+        }
+
+        [Test]
+        public void GetBillTotal_Three_Days_Of_Boarding_Gets_Discount()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsThreeDaysOfBoarding);
+            bill.getBillTotal().Should().Be(132.3M);
+        }
+
+        [Test]
+        public void GetBillTotal_Unknown_Customer_Item_Returns_Bill_Of_0()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsUnknownItem);
+            bill.getBillTotal().Should().Be(0);
+        }
+
+        [Test]
+        public void GetBillTotal_Single_Day_With_Coupon()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsSingleDayWithCoupon);
+            bill.getBillTotal().Should().Be(44);
+        }
+
+        [Test]
+        public void GetBillTotal_Three_Days_Of_Boarding_Gets_Discound_And_Coupon()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsTwoDaysWithCoupon);
+            bill.getBillTotal().Should().Be(127.3M, "(3 days*49)* 0.9 discount - 5 for coupon = 127.3");
+        }
+
+        [Test]
+        public void GetBillTotal_Multiple_Customer_Items_Out_Of_Order()
+        {
+            var bill = new FluffBill(serviceMenu1, customerItemsMultipleItems);
+
+            //0.9( 50 + 10 + 196) = 230.4
+            bill.getBillTotal().Should().Be(230.4M, "((5 days of food) * $10 + (2 chew toys) *$5 + (4 days of boarding) * $49) * 90% off total");
+        }
+    }
+}
--- FluffMonsters.Services/FluffBill.cs
+++ FluffMonsters.Services/FluffBill.cs
@@ -2,21 +2,107 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using FluffMonsters.Models;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace FluffMonsters.Services
 {
     public class FluffBill
     {
-        public FluffBill(JArray serviceMenu, JArray customerItems)
-        {
+        private Dictionary<string, decimal> _petServiceMenu;
+        private List<string> _customerBillItems;
+        private List<decimal> _customerCoupons;
+        private string _couponFormat = @"\$(\d+\.\d\d) coupon";
+        private Regex _couponRegex;
 
+        public FluffBill(string serviceMenu, string customerItems)
+        {
+            _couponRegex = new Regex(_couponFormat, RegexOptions.IgnoreCase);
+            _petServiceMenu = GetMenuFromJson(serviceMenu);
+            _customerBillItems = GetBillItemsFromJson(customerItems);
+            _customerCoupons = GetCouponsFromJson(customerItems);
         }
 
         public Decimal getBillTotal()
         {
-            return 0;
+            decimal total = 0;
+            foreach (var billItem in _customerBillItems)
+            {
+                if (_petServiceMenu.ContainsKey(billItem))
+                {
+                    total += _petServiceMenu[billItem];
+                }
+            }
+            ApplyDiscount(_customerBillItems, ref total);
+
+            //apply coupons after discount.
+            total -= _customerCoupons.Sum();
+            return total;
         }
+
+        private void ApplyDiscount(List<string> customerBillItems, ref decimal total)
+        {
+            const string discountItemString = "day of boarding";
+            //10% off discount if they have more than to days of boarding.
+            if (customerBillItems.Count(bi => bi.ToLower().Equals(discountItemString)) > 2)
+            {
+                total *= 0.9M;
+            }
+        }
+
+        private Dictionary<string, decimal> GetMenuFromJson(string serviceMenu)
+        {
+            var serviceMenuArray = JsonConvert.DeserializeObject<JArray>(serviceMenu);
+            var menu = new Dictionary<string, decimal>();
+            foreach (var menuItem in serviceMenuArray)
+            {
+                if (menuItem["name"] != null && menuItem["price"] != null)
+                {
+                    decimal price;
+                    string name = menuItem["name"].ToString().Trim();
+                    if (Decimal.TryParse(menuItem["price"].ToString().Trim().Trim('$'), out price))
+                    {
+                        menu.Add(name, price);
+                    }
+                }
+            }
+            return menu;
+        }
+
+        private List<string> GetBillItemsFromJson(string customerItems)
+        {
+            var customerItemsArray = JsonConvert.DeserializeObject<JArray>(customerItems);
+            List<string> items = new List<string>();
+            foreach (var customerItem in customerItemsArray)
+            {
+                if (customerItem.Type == JTokenType.String && !customerItem.ToString().ToLower().Contains("coupon"))
+                {
+                    items.Add(customerItem.ToString());
+                }
+            }
+            return items;
+        }
+
+        private List<decimal> GetCouponsFromJson(string customerItems)
+        {
+            var customerItemsArray = JsonConvert.DeserializeObject<JArray>(customerItems);
+            List<decimal> items = new List<decimal>();
+            foreach (var customerItem in customerItemsArray)
+            {
+                if (customerItem.Type == JTokenType.String && _couponRegex.IsMatch(customerItem.ToString()))
+                {
+                    var matches = _couponRegex.Matches(customerItem.ToString());
+                    if (matches.Count > 0 && matches[0].Groups.Count > 1)
+                    {
+                        items.Add(decimal.Parse(matches[0].Groups[1].Value));
+                    }
+                }
+            }
+            return items;
+        }
+
     }
 }
